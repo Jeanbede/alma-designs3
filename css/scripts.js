87e530ff6bac302b7c57ba0389e059a7503f6433
Jeanbede <njeanbeda@gmail.com>
2021-01-05 02:22:46 +0300

    adding files

---
+++ css/scripts.js
@@ -0,0 +1,34 @@
+var blink = document.getElementById('blink');
+setInterval(function() {
+    blink.style.opacity = (blink.style.opacity == 0 ? 1 : 0);
+}, 1500);
+
+
+
+
+
+
+$(document).ready(function() {
+$('.container') .hover(function() {
+    $(this)
+    .find( '.caption')
+    .stop()
+    .animate({bottom: '0'}, {duration: 1600, easing: 'easeOutQuart' });
+}, function() {
+$(this)
+.find('.caption')
+.stop()
+.animate({bottom: '-100px'}, {duration: 1600, easing: 'easeOutQuart'});
+
+}, function() {
+    $(this)
+    .find('.caption')
+    .stop()
+    .animate({bottom: '-200px'}, {duration: 1600, easing: 'easeOutQuart'});
+
+
+
+
+});
+
+});
